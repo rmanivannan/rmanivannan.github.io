82d9c86f329d2fa36c3dbbd7c9bccd9204ed3ada
Manivannan R <manivannan13tnj@gmail.com>
2022-06-24 01:10:44 +0530

    Update speedometer.js

--- demos/speedometer-reusable/js/speedometer.js
+++ demos/speedometer-reusable/js/speedometer.js
@@ -114,12 +114,12 @@ $.fn.myfunc = function (userPref) {
                   '-moz-transform    :rotate('+curDig+'deg)',
                 ].join(";");
         tempDiv += '<div class="nob '+dangCls+'" style="left:'+induCatorLinesPosTop+'px;top:'+induCatorLinesPosLeft+'px;'+tempDegInd+'"></div>';
-        induCatorNumbPosLeft = (this.defaultProperty.edgeRadius - induCatorNumbPosX) - (this.defaultProperty.numbH/2);
-        induCatorNumbPosTop  = (this.defaultProperty.edgeRadius - induCatorNumbPosY) - (this.defaultProperty.numbW/2);
+        induCatorNumbPosLeft = (this.defaultProperty.edgeRadius - induCatorNumbPosX) - (this.defaultProperty.numbW/2);
+        induCatorNumbPosTop  = (this.defaultProperty.edgeRadius - induCatorNumbPosY) - (this.defaultProperty.numbH/2);
 	tempDiv += '<div class="numb '+dangCls+'" style="text-align: center;left:'+ induCatorNumbPosTop +'px;top:'+induCatorNumbPosLeft+'px;">'+ curIndVal +'</div>';
       }else{
-        induCatorLinesPosLeft = (this.defaultProperty.edgeRadius - induCatorLinesPosX )-(this.defaultProperty.midNobH/2);
-        induCatorLinesPosTop = (this.defaultProperty.edgeRadius - induCatorLinesPosY)-(this.defaultProperty.midNobW/2);
+        induCatorLinesPosLeft = (this.defaultProperty.edgeRadius - induCatorLinesPosX )-(this.defaultProperty.midNobW/2);
+        induCatorLinesPosTop = (this.defaultProperty.edgeRadius - induCatorLinesPosY)-(this.defaultProperty.midNobH/2);
         var tempDegInd = [
                   'transform         :rotate('+curDig+'deg)',
                   '-webkit-transform :rotate('+curDig+'deg)',
